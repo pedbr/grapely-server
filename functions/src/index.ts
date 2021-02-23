7c5fb946a6fe2681ad3c9bf335c4a00960bae7ac
Pedro Brás <pedro.c.bras@gmail.com>
2021-02-23 19:57:21 +0100

    fixes endpoint routes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -52,43 +52,43 @@ app.post('/login', loginUser)
 
 //--------------USER ROUTES-------------//
 app.get('/user', firebaseAuth, getUser)
-app.patch('/user', firebaseAuth, editUser)
-app.get('/userWithWineries', firebaseAuth, getUserWithWineries)
+app.patch('/user/edit', firebaseAuth, editUser)
+app.get('/user/wineries', firebaseAuth, getUserWithWineries)
 
 //--------------WINERY ROUTES-------------//
 app.get('/winery', firebaseAuth, getMyWineries)
-app.post('/winery', firebaseAuth, addWinery)
-app.patch('/winery/:wineryId', editWinery)
-app.delete('/winery/:wineryId', deleteWinery)
+app.post('/winery/add', firebaseAuth, addWinery)
+app.patch('/winery/edit/:wineryId', editWinery)
+app.delete('/container/delete/:wineryId', deleteWinery)
 
 //--------------CONTAINER ROUTES-------------//
 app.get('/container/winery/:wineryId', getWineryContainers)
-app.post('/container', firebaseAuth, addContainer)
-app.patch('/container/:containerId', editContainer)
-app.delete('/container/:containerId', deleteContainer)
+app.post('/container/add', firebaseAuth, addContainer)
+app.patch('/container/edit/:containerId', editContainer)
+app.delete('/container/delete/:containerId', deleteContainer)
 
 //--------------BATCH ROUTES-------------//
 app.get('/batch/container/:containerId', getContainerBatches)
-app.post('/batch', firebaseAuth, addBatch)
-app.patch('/batch/:batchId', editBatch)
-app.delete('/batch/:batchId', deleteBatch)
+app.post('/batch/add', firebaseAuth, addBatch)
+app.patch('/batch/edit/:batchId', editBatch)
+app.delete('/batch/delete/:batchId', deleteBatch)
 
 //--------------TASK ROUTES-------------//
 app.get('/task', firebaseAuth, getMyTasks)
 app.get('/task/winery/:wineryId', getWineryTasks)
 app.get('/task/container/:containerId', getContainerTasks)
 app.get('/task/batch/:batchId', getBatchTasks)
-app.post('/task', firebaseAuth, addTask)
-app.patch('/task/:taskId', editTask)
-app.delete('/task/:taskId', deleteTask)
+app.post('/task/add', firebaseAuth, addTask)
+app.patch('/task/edit/:taskId', editTask)
+app.delete('/task/delete/:taskId', deleteTask)
 
 //--------------NOTE ROUTES-------------//
 app.get('/note/winery/:wineryId', getWineryNotes)
 app.get('/note/container:containerId', getContainerNotes)
 app.get('/note/batch/:batchId', getBatchNotes)
 app.get('/note/task/:taskId', getTaskNotes)
-app.post('/note', firebaseAuth, addNote)
-app.patch('/note/:noteId', editNote)
-app.delete('/note/:noteId', deleteNote)
+app.post('/note/add', firebaseAuth, addNote)
+app.patch('/note/edit/:noteId', editNote)
+app.delete('/note/delete/:noteId', deleteNote)
 
 exports.api = functions.region('europe-west1').https.onRequest(app)
